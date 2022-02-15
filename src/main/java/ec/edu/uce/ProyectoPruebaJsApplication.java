d2ba11911e4989226d040c948277137bfad559f0
Jorge Sanchez <jlsanchez1997@hotmail.es>
2022-02-14 20:12:29 -0500

    correcion prueba unidad 2

---
+++ src/main/java/ec/edu/uce/ProyectoPruebaJsApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPruebaJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPruebaJsApplication.class, args);
+	}
+
+}
